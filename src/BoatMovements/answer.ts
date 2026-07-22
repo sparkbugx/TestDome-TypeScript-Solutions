4cfb79386ef54d204dedd5c48ab07738e36a7eb8
Dumpling <dumplingone@proton.me>
2026-07-22 07:07:30 -0600

    Initial code for the Boat Movements exercise added

--- src/BoatMovements/answer.ts
+++ src/BoatMovements/answer.ts
@@ -1,1 +1,17 @@
+function canTravelTo(gameMatrix: boolean[][], fromRow: number, fromColumn: number, toRow: number, toColumn: number): boolean {
+    // Write your code here
+    return false;
+}
 
+const gameMatrix = [
+    [false, true,  true,  false, false, false],
+    [true,  true,  true,  false, false, false],
+    [true,  true,  true,  true,  true,  true],
+    [false, true,  true,  false, true,  true],
+    [false, true,  true,  true,  false, true],
+    [false, false, false, false, false, false],
+];
+
+console.log(canTravelTo(gameMatrix, 3, 2, 2, 2)); // true, Valid move
+console.log(canTravelTo(gameMatrix, 3, 2, 3, 4)); // false, Can't travel through land
+console.log(canTravelTo(gameMatrix, 3, 2, 6, 2)); // false, Out of bounds
